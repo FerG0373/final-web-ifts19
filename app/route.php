8fab231581a17d139f38671f7f72377e2601e483
FerG0373 <fer.dg.sc@gmail.com>
2025-07-16 15:55:09 -0300

    Probando archivo de rutas.

--- app/route.php
+++ app/route.php
@@ -5,3 +5,24 @@ $vista_login = 'login';
 // No hay lógica de base de datos ni POST de login todavía.
 // Solo define qué vista debe cargar index.php.
 ?>
+
+<?php
+// routes.php
+$routes = [
+    '/' => 'home.php',
+    '/login' => 'login.php',
+    '/home' => 'home.php'
+];
+
+// Obtener la ruta solicitada
+$request_uri = strtok($_SERVER['REQUEST_URI'], '?'); // Ignorar parámetros GET
+
+// Verificar si la ruta existe
+if (array_key_exists($request_uri, $routes)) {
+    require_once $routes[$request_uri];
+} else {
+    // Página no encontrada
+    header("HTTP/1.0 404 Not Found");
+    require_once '404.php';
+}
+?>
