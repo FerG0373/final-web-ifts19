dfebcb4848fe97cf01eba2083fba1098fbfa3774
FerG0373 <fer.dg.sc@gmail.com>
2025-07-30 15:58:06 -0300

    Estilos nav

--- app/views/_partials/_header.php
+++ app/views/_partials/_header.php
@@ -17,4 +17,17 @@ $menuTitulos = obtieneTitulosMenu($conexion);
     </ul>
 </nav>
 
+
+<nav class="main-nav">
+    <ul class="nav-list">
+        <?php foreach ($menuTitulos as $titulo): ?>
+            <li class="nav-item">
+                <a href="index.php?page=<?php echo htmlspecialchars($titulo['ruta_destino']); ?>" class="nav-link">
+                    <?php echo htmlspecialchars($titulo['descripcion']); ?>
+                </a>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+</nav>  
+
 <!-- htmlspecialchars() es una función de PHP que sirve para convertir caracteres especiales en entidades HTML. Es para seguridad.-->
