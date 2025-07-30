91d26d966e1bd23afa0247be16460b4566385ccf
FerG0373 <fer.dg.sc@gmail.com>
2025-07-30 00:13:56 -0300

    Formulario login terminado

--- app/views/1.00-login.php
+++ app/views/1.00-login.php
@@ -1,55 +1,23 @@
 <div class="login-container">
     <form class="login-form" action="index.php?page=/login" method="POST">
-        <h2 class="title">¡Bienvenido!</h2>
-        
+        <h2 class="form-title">¡Bienvenido!</h2>        
         <div class="form-group">
             <label class="form-label" for="username">Usuario:</label>
             <input class="form-input" type="text" id="username" name="username" placeholder="Usuario" required>
-        </div>
-        
+        </div>        
         <div class="form-group password-container">
             <label class="form-label" for="pass">Contraseña:</label>
             <input class="form-input password-input" type="password" id="pass" name="pass" placeholder="Contraseña" required>
             <span class="material-symbols-outlined password-toggle" onclick="alternarVisibilidad()">visibility_off</span>
-        </div>
-        
-        <?php if (isset($_SESSION['login_error'])): ?>
-        <div class="mensaje error">
-            <?php echo($_SESSION['login_error']); ?>
-        </div>
-        <?php unset($_SESSION['login_error']); ?>
+        </div>        
+        <?php
+            // Muestra mensaje de error según corresponda.
+            if (isset($_SESSION['login_error'])): ?>
+            <div class="error-message">
+                <?php echo($_SESSION['login_error']); ?>
+            </div>
+            <?php unset($_SESSION['login_error']); ?>
         <?php endif; ?>
-
         <button class="submit-btn" type="submit" name="login">Iniciar Sesión</button>
     </form>
 </div>
-
-
-
-
-
-
-<h2>¡Bienvenido!</h2>
-
-<form action="index.php?page=/login" method="POST"> 
-    <div>
-        <label for="username">Usuario:</label>
-        <input type="text" id="username" name="username" placeholder="Usuario" required>
-    </div>
-    <div>
-        <label for="pass">Contraseña:</label>
-        <input type="password" id="pass" name="pass" placeholder="Contraseña" required>
-        <span class="material-symbols-outlined" onclick="alternarVisibilidad()">visibility_off</span>
-    </div>
-    
-    <?php 
-        // Muestra mensaje de error según corresponda.
-        if (isset($_SESSION['login_error'])): ?>
-        <div class="mensaje error">
-            <?php echo($_SESSION['login_error']); ?>
-        </div>
-        <?php unset($_SESSION['login_error']); ?>
-    <?php endif; ?>
-
-    <button type="submit" name="login">Iniciar Sesión</button>
-</form>
