c59f8d82c58386a797b66564214db29b88066c9d
FerG0373 <fer.dg.sc@gmail.com>
2025-07-29 23:34:03 -0300

    Variables css; formulario login

--- app/views/1.01-home.php
+++ app/views/1.01-home.php
@@ -1,6 +1,5 @@
-
 <section>
-    <img src="../public/assets/img/ifts19_logo.png" alt="">
+    <img src="../public/assets/img/ifts19_logo.png" alt="Logo del IFTS 19">
     <h1>Desarrollo de Aplicaciones Web</h1>
     <h2>Trabajo Práctico Final</h2>
     <p>2025</p>
--- app/views/_partials/_headerLogin.php
+++ app/views/_partials/_headerLogin.php
@@ -1,14 +1,14 @@
 <nav class="nav-login">
-    <div class="nav-login__contenedor">
-        <div class="nav-login__elementos">
-            <img src="../public/assets/img/ifts19_logo.png" alt="Logo de IFTS 19" class="nav-login__logo">
-            <p class="nav-login__titulo">Final desarrollo de Aplicaciones WEB - 2025</p>
+    <div class="nav-login-container">
+        <div class="group-nav-elements">
+            <img src="../public/assets/img/ifts19_logo.png" alt="Logo de IFTS 19" class="nav-login-logo">
+            <p class="nav-title">Final desarrollo de Aplicaciones WEB - 2025</p>
         </div>        
-        <div class="nav-login__elementos">
-            <button type="button" aria-label="Cambiar tema" class="nav-login__tema">
+        <div class="group-nav-elements">
+            <button type="button" aria-label="Cambiar tema" class="nav-login-theme">
                 🌓
             </button>    
-            <div class="nav-login__fecha">
+            <div class="nav-date">
                 <span id="fecha-actual">Fecha Actual</span>
             </div>
         </div>
